0f97cc031c7bc4005cafc4b08f231e0ab51cdc72
Andrés Murray <andymurray99@hotmail.com>
2025-02-26 17:46:43 -0300

    configuro proyecto de inicio

--- negocio/AccesoDatos.cs
+++ negocio/AccesoDatos.cs
@@ -19,7 +19,8 @@ namespace negocio
 
             public AccesoDatos()
             {
-                conexion = new SqlConnection("server= .\\SQLEXPRESS; database=CATALOGO_DB; integrated security =true");
+    
+            conexion = new SqlConnection("server= .\\SQLEXPRESS; database=CATALOGO_DB; integrated security =true");
                 comando = new SqlCommand();
             }
 
